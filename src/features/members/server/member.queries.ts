807517a920095bb3c6a873e7ce504dc4017f4f37
KunalPandey-675 <pandeykunal2084@gmail.com>
2026-03-26 19:16:21 +0530

    implemented task and team creation logic along with frontend

--- src/features/members/server/member.queries.ts
+++ src/features/members/server/member.queries.ts
@@ -13,3 +13,36 @@ export async function fetchUserById(id: any) {
 
   return data;
 }
+
+export async function fetchUsers() {
+  const supabase = createSupabaseClient();
+  const { data } = await supabase
+    .from("users")
+    .select(`
+    id,
+    name`);
+  return data
+}
+
+export async function fetchUsersByTeamId(teamId: string) {
+  const supabase = createSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("users")
+    .select(`
+      id,
+      name,
+      team_member!inner(team_id)
+    `)
+    .eq("team_member.team_id", teamId);
+
+  if (error) {
+    console.error("fetchUsersByTeamId failed:", error.message);
+    return [];
+  }
+
+  return (data ?? []).map((user) => ({
+    id: user.id,
+    name: user.name,
+  }));
+}
